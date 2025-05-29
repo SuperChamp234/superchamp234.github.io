0172d8864bc82093d502637e65ca2c2d0929e97d
Zain <beingzainsv@gmail.com>
2025-05-29 18:07:27 +0530

    Add new styles for home and post pages, update gallery layout, and implement typing animation
    - Introduced a new CSS file for home page styles, including navbar and hero section.
    - Created a new CSS file for post page styles, enhancing the layout and typography.
    - Updated gallery styles to improve container width and adjusted image height.
    - Added JavaScript for typing animation using Typed.js library.
    - Included a minified version of Typed.js for performance.
    - Added a landscape photo to the media directory.
    - Marked the about me post as a draft for future editing.

---
+++ js/home.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        strings: [
+            'Zain Siddavatam',
+            'an engineer',
+            'a passionate developer',
+            'a f1 fan, cat lover, mountain enjoyer',
+        ],
+        typeSpeed: 60,
+        backSpeed: 40,
+        backDelay: 1800,
+        startDelay: 600,
+        loop: true,
+        showCursor: true,
+        cursorChar: '|',
+        smartBackspace: true
+    };
+    
+    var typed = new Typed('#typed', options);
+});
